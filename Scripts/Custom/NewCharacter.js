a85dce5c8cb2a2f55dc768c384dfc2bc49ce1841
Nick Spears <spears.ns@gmail.com>
2021-11-25 22:00:24 -0700

    New Character needed slight debug

--- Scripts/Custom/NewCharacter.js
+++ Scripts/Custom/NewCharacter.js
@@ -585,108 +585,7 @@
 		d.resolve();
 		return d.promise()
 	}
-	/*
-	function getBackgroundSkills() {
-		d = new $.Deferred();
-		// HARD-CODED FOR STANDARD SKILLS
-		var combatSlot = 10;
-		var socialSlot = 5;
-		var covertSlot = 4;
-		var survivalSlot = 4;
-		var medicalSlot = 3;
-		var scienceSlot = 1;
-		var craftsmanSlot = 2;
-		var constructionSlot = 2;
-		var technologySlot = 1;
-		var transportationSlot = 1;
-
-		if (typeof skillsArr != "undefined" && Array.isArray(skillsArr) && skillsArr.length > 0) {
-			for (i = 0; i < skillsArr.length; i++) {
-				var skill = skillsArr[i];
-				console.log(skill);
-				$.ajax({
-					type: 'POST',
-					traditional: true,
-					url: '/Characters/GetSkillData',
-					data: '{Name: "' + skill + '" }',
-					dataType: 'json',
-					contentType: "application/json; charset=utf-8",
-					success:
-						function (results) {
-							for (i = 0; i < results.length; i++) {
-
-								var skillName = results.Name;
-								var skillType = results.Type;
-								var skillClass = results.Class;
-								var shortTxt = results.ShortTxt;
-								var longTxt = results.LongTxt;
-								var skillFormula = results.Formula;
-
-								var slotNum;
-								if (skillClass == "Combat") slotNum = combatSlot;
-								else if (skillClass == "Affiliation") { skillClass = "Social"; slotNum = socialSlot; }
-								else if (skillClass == "Languages") { skillClass = "Social"; slotNum = socialSlot; }
-								else if (skillClass == "Social") slotNum = socialSlot;
-								else if (skillClass == "Covert") slotNum = covertSlot;
-								else if (skillClass == "Survival") slotNum = survivalSlot;
-								else if (skillClass == "Craftsman") slotNum = craftsmanSlot;
-								else if (skillClass == "Construction") slotNum = constructionSlot;
-								else if (skillClass == "Medical") slotNum = medicalSlot;
-								else if (skillClass == "Science") slotNum = scienceSlot;
-								else if (skillClass == "Technology") slotNum = technologySlot;
-								else if (skillClass == "Transportation") slotNum = transportationSlot;
-
-								var border;
-								var training;
-								if (skillType == "General") { border = "border-warning"; training = "trainGenBtn"; }
-								else if (skillType == "Advanced") { border = "border-danger"; training = "trainAdvBtn"; }
-								else { border = "border-secondary"; training = "trainGenBtn"; } // skillClass == "Focus"
-
-								var nextSlot
-								if (skillClass == "Combat") nextSlot = slotNum + 2;
-								else nextSlot = slotNum + 1;
-
-								var skillValue = eval(skillFormula) + roll(1, 10);
-
-								console.log("BG Skill: " + skillName + " => " + skillValue);
-								// RENDER HTML FOR NEW SKILL
-								$("#" + skillClass.toLowerCase() + "-" + slotNum).html(
-									'<div class="input-group my-0">' +
-									'<button class="btn btn-block border ' + border + ' bw-thick font-weight-bold my-1 px-0 ' + training + ' " data-skill="' + skillName + '" data-skillclass="' + skillClass + '" data-shortTxt="' + shortTxt + '" data-longTxt="' + longTxt + '" type="button">' + skillName + '</button>' +
-									'</div>'
-								);
-								$("#" + skillClass.toLowerCase() + "Val-" + slotNum).html(
-									'<div class="input-group my-2">' +
-									'<input class="form-control text-center px-0 py-0" type="number" name="Skill-' + skillName + '" value="' + skillValue + '" readonly />' +
-									'</div>'
-								);
-								// RENDER HTML FOR NEW ADD SKILL BTN
-								$("#" + skillClass.toLowerCase() + "-" + nextSlot).html(
-									'<button class="btn btn-block btn-info border border-dark text-center font-weight-bold addSkillBtn my-2 mx-auto" data-target="' + skillClass + "Skills" + '" type="button">ADD</button>'
-								);
-								$("#" + skillClass.toLowerCase() + "Val-" + nextSlot).html(
-									'<div class="input-group my-2">' +
-									'<input class="form-control text-center px-0 py-0" value="" readonly />' +
-									'</div>'
-								);
-								// INCREASE COUNT ON APPROPRIATE SLOT
-								if (skillClass == "Combat") combatSlot += 1;
-								else if (skillClass == "Social") socialSlot += 1;
-								else if (skillClass == "Covert") covertSlot += 1;
-								else if (skillClass == "Survival") survivalSlot += 1;
-								else if (skillClass == "Medical") medicalSlot += 1;
-								else if (skillClass == "Science") scienceSlot += 1;
-								else if (skillClass == "Technology") technologySlot += 1;
-								else if (skillClass == "Transportation") transportationSlot += 1;
-							}
-						}
-				});
-            }
-		}
-		d.resolve();
-		return d.promise()
-	}
-	*/
+	
 	// CLIENT SIDE SKILL SELECTIONS
 	// TRAIN GEN BUTTONS
 	$("body").on("click", ".trainGenBtn", function () {
@@ -832,7 +731,7 @@
 		// GET JSON RETURN & RENDER HTML
 		$.ajax({
 			type: 'POST',
-			url: 'Characters/GetNewSkills',
+			url: '/Characters/GetNewSkills',
 			data: '{SkillClass: "' + skillClass + '"}',
 			dataType: 'json',
 			contentType: "application/json; charset=utf-8",
